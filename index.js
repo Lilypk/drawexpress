c448e7a386f1c038f0d11a3d428932837d2bfc15
LILYKLOPCIC <LILYKLOPCIC@GMAIL.COM>
2020-12-17 13:09:52 -0500

    deply2

--- index.js
+++ index.js
@@ -32,11 +32,7 @@ app.use(
 		credentials: true
 	})
 );
-app.use(function (req, res, next) 
-{	res.header("Access-Control-Allow-Methods", "POST,GET,PATCH,PUT,DELETE");	
-res.header(		"Access-Control-Allow-Headers",		"Origin, X-Requested-With, Content-Type, Accept"	);	
-res.header("Access-Control-Allow-Credentials", "true");	
-res.header("Access-Control-Allow-Origin", "http://localhost:3000");	next();});
+
 
 // Middleware
 app.use(bodyParser.json());
